b4fd23e355fee12e718a16ac09fa436d4d042439
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 17:02:07 +0800

    Add files via upload

---
+++ CSharpAdvancedProgramming/MyException/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                throw new ValidationExcepion("now i try to throw a validation exception");
+                //throw new Exception("a Exception");
+            }
+            catch (ValidationExcepion ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey(); 
+        }
+    }
+
+
+    public class ValidationExcepion : Exception
+    {
+        public ValidationExcepion() : base("a new ValidationExcepion") { }
+        public ValidationExcepion(string message):base(message)
+        {
+            
+        }
+
+
+    }
+
+}
