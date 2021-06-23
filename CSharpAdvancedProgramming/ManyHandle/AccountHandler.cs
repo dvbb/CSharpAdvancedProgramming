ad37d102bb5d89ae0b4820a3d77fa59b357c6690
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 16:54:10 +0800

    Add files via upload

---
+++ CSharpAdvancedProgramming/ManyHandle/AccountHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyHandle
+{
+    public class AccountHandler : CommandHandlerBase<string,string>
+    {
+        public override Task<string> Handle(string request)
+        {
+            Console.WriteLine("user ");
+            return Task.FromResult("ss");
+        }
+    }
+}
---
+++ CSharpAdvancedProgramming/ManyHandle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManyHandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
