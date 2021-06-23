a8b57849a302261be37d7f628ee431a69d916940
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 17:07:01 +0800

    Add files via upload

---
+++ CSharpAdvancedProgramming/NlogDemo/LogType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NlogDemo
+{
+    public enum LogType
+    {
+        All,
+        Information,
+        Debug,
+        Success,
+        Failure,
+        Warning,
+        Error
+    }
+}
