c32e3e4c15b220c55f45a5faee4c51002776147b
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 17:09:18 +0800

    Classes under test

---
+++ CSharpAdvancedProgramming/MyTest/IStudentProvider.cs
@@ -0,0 +1,14 @@
+﻿using MyTest.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+   public interface IStudentProvider
+    {
+        Task<GetStudentTop20Response> GetStudentsTop20(GetStudentRequest request);
+
+    }
+}
---
+++ CSharpAdvancedProgramming/MyTest/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTest
+{
+    public interface IStudentRepository
+    {
+        Student GetStudentBySno(string Sno);
+        IEnumerable<Student> GetStudent();
+        void InsertStudent(string Sno, string Sname);
+    }
+}
