c8e6d58ff65f77b19d8c67bbf1f0078016b46c20
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 17:00:15 +0800

    Add files via upload

---
+++ CSharpAdvancedProgramming/MyConfig/GetValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyConfig
+{
+    public static class GetValue
+    {
+        public static string GetValueDvbb()
+        {
+
+            string filepath = System.Reflection.Assembly.GetExecutingAssembly().CodeBase.Replace(@"file:///", string.Empty) + ".config";
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(filepath);
+            string data = xDoc.SelectSingleNode(@"/configuration/appSettings/add[@key='dvbb']").Attributes["value"].Value;
+
+            return data;
+        }
+    }
+}
