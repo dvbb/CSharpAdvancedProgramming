5dc7e54a7432d4b1259cac96d545b45b8b6712ae
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 17:10:18 +0800

    Unit Test
    Unit Test

---
+++ CSharpAdvancedProgramming/MyTestTests/CSCalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace MyTest.Tests
+{
+    public class CSCalculatorTests
+    {
+        private IStudentProvider _provider;
+        private Mock<IStudentRepository> _studentRepository;
+        
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _studentRepository = new Mock<IStudentRepository>();
+            _provider = new StudentProvider(_studentRepository.Object);
+        }
+
+        [Test]
+        public async Task Test_MymoqTest_Success_With_None()
+        {
+            var request = new GetStudentRequest { 
+                ProgramCode = "cbr",
+                Sno = "20171316",
+                RequestHeader = new RequestHeader {
+                    RequestId = Guid.Empty,
+                }
+            };
+            _studentRepository.Setup(x => x.GetStudent())
+                .Returns(GetStudentList());
+            var result =await _provider.GetStudentsTop20(request);
+            Assert.IsNotNull(result);
+        }
+
+        public List<Student> GetStudentList()
+        {
+            List<Student> result = new List<Student>();
+            result.Add(new Student
+            {
+                Sno = "20171316",
+                Sname = "dvbb",
+            });
+            result.Add(new Student
+            {
+                Sno = "20170001",
+                Sname = "hibi",
+            });
+            return result;
+        }
+    }
+}
