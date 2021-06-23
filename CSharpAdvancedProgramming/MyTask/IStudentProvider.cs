47766a18cccd88c504781e10ae993c754c06aa7e
dvbb <61542202+dvbb@users.noreply.github.com>
2021-06-23 17:05:11 +0800

    Add files via upload

---
+++ CSharpAdvancedProgramming/MyTask/IStudentProvider.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTask
+{
+    public interface IStudentProvider
+    {
+        Task<List<Student>> GetStudent(JArray resource);
+        List<Student> Sync_Student(JArray resource);
+    }
+}
